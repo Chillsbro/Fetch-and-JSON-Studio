e2ca8019a6b3e9384d3e8bd5688a7950cd5a439f
Chillsbro <114181571+Chillsbro@users.noreply.github.com>
2023-07-24 18:26:08 -0500

    add api functionality and proper html structure

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-// TODO: add code here
+window.addEventListener("load", function () {
+  fetch(
+    "https://handlers.education.launchcode.org/static/astronauts.json"
+  ).then((response) => {
+    response.json().then((data) => {
+      const container = document.getElementById("container");
+      let count = data.length;
+      container.innerHTML = `<h2>${count}</>`
+      data.forEach((astro) => {
+        container.innerHTML += `
+        <div id="container">
+        <div class="astronaut">
+        <div class="bio">
+        <h3>${astro.firstName} ${astro.lastName}</h3>
+        <ul>
+        <li>Hours in space: ${astro.hoursInSpace} </li>
+        <li>Active: ${astro.active}</li>
+        <li>Skills: ${astro.skills}</li>
+        </ul>
+        </div>
+        <img src="${astro.picture}" class="avatar"/>
+        </div>
+      </div> `;
+       
+      });
+    });
+  });
+});
