f1bf4ad9c6ce7acc71ac2c05096d0e7b9f3ec2b3
Chillsbro <114181571+Chillsbro@users.noreply.github.com>
2023-07-24 20:20:18 -0500

    add logic for numerical sorting of astronauts

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 window.addEventListener("load", function () {
   fetch(
     "https://handlers.education.launchcode.org/static/astronauts.json"
-  ).then((response) => {
-    response.json().then((data) => {
+  ).then(function (response) {
+    response.json().then(function (data) {
+      let sortedAstronatus = data.sort((a,b) =>  b.hoursInSpace - a.hoursInSpace);
       const container = document.getElementById("container");
       let count = data.length;
-      container.innerHTML = `<h2>${count}</>`
-      data.forEach((astro) => {
+      container.innerHTML = `<h3>${count}</h3>`;
+      console.log(sortedAstronatus);
+      sortedAstronatus.forEach((astro) => {
         container.innerHTML += `
-        <div id="container">
-        <div class="astronaut">
-        <div class="bio">
-        <h3>${astro.firstName} ${astro.lastName}</h3>
-        <ul>
-        <li>Hours in space: ${astro.hoursInSpace} </li>
-        <li>Active: ${astro.active}</li>
-        <li>Skills: ${astro.skills}</li>
-        </ul>
-        </div>
-        <img src="${astro.picture}" class="avatar"/>
-        </div>
-      </div> `;
-       
+                  <div class="astronaut">
+                  <div class="bio">
+                  <h3>${astro.firstName} ${astro.lastName}</h3>
+                  <ul>
+                  <li>Hours in space: ${astro.hoursInSpace} </li>
+                  <li>Active:${astro.active}</li>
+                  <li>Skills: ${astro.skills}</li>
+                  </ul>
+                  </div>
+                  <img src="${astro.picture}" class="avatar"/>
+                  </div>
+                  `;
       });
     });
   });
